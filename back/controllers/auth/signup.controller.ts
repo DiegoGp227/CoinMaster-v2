e26284867f18b9b8165b4259142ae110370c6154
DiegoGp227 <diego.gp227@gmail.com>
2025-10-25 15:21:14 -0500

    [ feat ] signup integrated

---
+++ back/controllers/auth/signup.controller.ts
@@ -0,0 +1,57 @@
+import bcryptjs from "bcryptjs";
+import db from "../../db/db.js";
+import jwt, { type Secret } from "jsonwebtoken";
+import type ms from "ms";
+
+const JWT_SECRET = process.env.JWT_SECRET as Secret;
+const TOKEN_EXPIRATION = (process.env.TOKEN_EXPIRATION || "1h") as ms.StringValue;
+
+
+
+const signup = async ({ body, set }: { body: any; set: any }) => {
+  const { name, email, password } = body;
+
+  if (!email || !password) {
+    set.status = 400;
+    return { message: "Email and password are required." };
+  }
+
+  try {
+    console.log("Consultando usuario con email:", email);
+    const [rows]: [any[], any] = await db.execute("SELECT * FROM users WHERE email = ?", [email]);
+
+    if (rows.length > 0) {
+      console.log("El usuario ya existe");
+      set.status = 409;
+      return { message: "User already exists." };
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    console.log("Contraseña encriptada");
+
+    const insertQuery = "INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)";
+    const [insertResult]: any = await db.execute(insertQuery, [name || null, email, hashedPassword]);
+
+    console.log("Usuario creado exitosamente con ID:", insertResult.insertId);
+
+    const token = jwt.sign({ id: insertResult.insertId, email }, JWT_SECRET, {
+      expiresIn: TOKEN_EXPIRATION,
+    });
+
+    set.status = 201;
+    return {
+      message: "User successfully created.",
+      userId: insertResult.insertId,
+      token,
+      email,
+      name: name || null,
+    };
+  } catch (error) {
+    console.error("Error en la operación:", error);
+    set.status = 500;
+    return { message: "Internal server error." };
+  }
+};
+
+export default signup;
--- back/routes/index.routes.ts
+++ back/routes/index.routes.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
 import dbCheck from "../controllers/test/test";
+import signup from "../controllers/auth/signup.controller";
 
 export const router = new Elysia({ prefix: "/api" })
+  //Auth
+  //   .post("/login", login)
+  .post("/signup", signup)
+
   .get("/db", dbCheck)
   .get("/back", () => "Test route is working");
