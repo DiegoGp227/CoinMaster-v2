7d2703ae4f2ff7d9443fc676ebd4bb125ebfd8a1
DiegoGp227 <diego.gp227@gmail.com>
2025-10-25 15:58:51 -0500

    [ init ] front inicialiced

---
+++ front/app/layout.tsx
@@ -0,0 +1,41 @@
+import { SWRProvider } from "@/provier/swrProvider";
+import "../styles/global.css";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+// import Header from "./components/organisms/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Taskly",
+  description: "Taskly app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-hard-gray min-h-screen flex flex-col`}
+      >
+        <SWRProvider>
+          {/* <Header /> */}
+          <div className="max-w-screen-2xl mx-auto flex-1 w-full">
+            {children}
+          </div>
+        </SWRProvider>
+      </body>
+    </html>
+  );
+}
---
+++ front/src/shared/constants/urls.ts
@@ -0,0 +1,2 @@
+export const BaseURL =
+  process.env.NEXT_PUBLIC_API_URL || "http://192.168.0.3:4000";
