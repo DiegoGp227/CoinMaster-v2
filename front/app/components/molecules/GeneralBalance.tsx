6c8d3949ec7580022dd80d73aad2f6c3767e2dba
DiegoGp227 <diego.gp227@gmail.com>
2025-11-08 10:47:11 -0500

    [ feat ] changes responsive in the general balance

--- front/app/components/molecules/GeneralBalance.tsx
+++ front/app/components/molecules/GeneralBalance.tsx
@@ -1,15 +1,15 @@
 export default function GeneralBalance() {
   return (
-    <div className="flex justify-center gap-5 my-4">
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
+    <div className="flex justify-center gap-5 my-4 flex-wrap">
+      <div className="w-[270px] h-[140px] bg-black rounded-[5px] flex flex-col justify-center items-center">
         <p className="text-5xl text-white">Income</p>
         <p className="text-4xl text-income">2.000.000</p>
       </div>
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
+      <div className="w-[270px] h-[140px] bg-black rounded-[5px] flex flex-col justify-center items-center">
         <p className="text-5xl text-white">Expenses</p>
         <p className="text-4xl text-expense">1.300.000</p>
       </div>
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
+      <div className="w-[270px] h-[140px] bg-black rounded-[5px] flex flex-col justify-center items-center">
         <p className="text-5xl text-white">Balance</p>
         <p className="text-4xl text-income">700.000</p>
       </div>
