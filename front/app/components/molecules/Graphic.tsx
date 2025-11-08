3721c4b773be8feec8a7f30dd725dfdf7fe729a4
DiegoGp227 <diego.gp227@gmail.com>
2025-11-08 12:15:26 -0500

    [ style ] graphics style corrected

--- front/app/components/molecules/Graphic.tsx
+++ front/app/components/molecules/Graphic.tsx
@@ -5,9 +5,11 @@ interface IGraphicProps {
 export default function Graphic({ title }: IGraphicProps) {
   return (
     <div className="flex justify-center my-4">
-      <div className="w-[84%] h-[300px] bg-black justify-center rounded-[5px] p-5">
+      <div className="w-[84%] h-[300px] bg-black justify-center rounded-[5px] p-5 border-2 hover:border-soft-gray transition-colors duration-500 group">
         <div className="flex justify-center">
-          <p className="text-white text-2xl">{title}</p>
+          <p className="text-soft-gray text-2xl group-hover:text-white transition-colors duration-500 group">
+            {title}
+          </p>
         </div>
         <div></div>
       </div>
