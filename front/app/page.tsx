d9eff9a5223bafff020a64658f8e1eff4ebb36b8
DiegoGp227 <diego.gp227@gmail.com>
2025-11-08 00:37:53 -0500

    [ feat ] dashboard organism integrated

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,18 +1,5 @@
+import DashboardOrganism from "./components/organisms/DashboardOrganism";
+
 export default function HomePage() {
-  return (
-    <div className="flex justify-center gap-5 mt-8">
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
-        <p className="text-5xl text-white">Income</p>
-        <p className="text-4xl text-income">2.000.000</p>
-      </div>
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
-        <p className="text-5xl text-white">Expenses</p>
-        <p className="text-4xl text-expense">1.300.000</p>
-      </div>
-      <div className="w-[300px] h-[200px] bg-black rounded-[5px] flex flex-col justify-center items-center">
-        <p className="text-5xl text-white">Balance</p>
-        <p className="text-4xl text-income">700.000</p>
-      </div>
-    </div>
-  );
+  return <DashboardOrganism />;
 }
