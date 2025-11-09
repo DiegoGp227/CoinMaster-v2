6e5495255359f9e7fe6590f7e56130fe05bb31a4
DiegoGp227 <diego.gp227@gmail.com>
2025-11-09 15:52:31 -0500

    [ feat ] modal integrated

--- front/app/transactions/components/organisms/TransactionsOrganism.tsx
+++ front/app/transactions/components/organisms/TransactionsOrganism.tsx
@@ -2,20 +2,43 @@
 import FilterDashboard from "@/app/components/molecules/FilterDashboard";
 import { FilterOption } from "@/app/components/organisms/DashboardOrganism";
 import { useState } from "react";
-import { LuCross } from "react-icons/lu";
+import Modal from "@/app/components/atoms/Modal";
+import NewTranssactions from "../atoms/NewTransactions";
+import TableDiv from "../molecules/TableDiv";
 
 export default function TransactionsOrganism() {
   const [filterDate, setFilterDate] = useState<FilterOption>("Month");
+  const [modalNewTransactions, setModalNewTransactions] =
+    useState<boolean>(false);
 
   return (
     <>
       <FilterDashboard setFilterDate={setFilterDate} filterDate={filterDate} />
-      <div className="flex justify-center">
-        <button className="w-[320px] h-[140px] bg-black rounded-[5px] flex justify-center items-center border-2 hover:border-soft-gray hover:shadow-[0_0_10px_2px_rgba(156,163,175,0.3)] group transition-all duration-500 gap-3">
-          <p className="text-white text-3xl">New Transaction</p>
-          <LuCross className="text-white w-7 h-7" />
-        </button>
+      <NewTranssactions
+        setModalNewTransactions={() => setModalNewTransactions(true)}
+      />
+      <TableDiv title="History">
+        <div></div>{" "}
+      </TableDiv>
+
+      <div className="flex justify-center my-4">
+        <div className="w-[84%] h-[400px] bg-black justify-center rounded-[5px] p-5 border-2 hover:border-soft-gray hover:shadow-[0_0_10px_2px_rgba(156,163,175,0.3)] transition-all duration-500 group">
+          <div className="flex justify-center mb-4">
+            <p className="text-soft-gray text-2xl group-hover:text-white transition-all duration-500 group">
+              {"title"}
+            </p>
+          </div>
+          <div className="w-full h-[300px]">{"children"}</div>
+        </div>
       </div>
+
+      {modalNewTransactions ? (
+        <Modal onClose={() => setModalNewTransactions(false)}>
+          <div>h2sdasda</div>
+        </Modal>
+      ) : (
+        ""
+      )}
     </>
   );
 }
