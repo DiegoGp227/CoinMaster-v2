e1093dac317bdb1437e4e04441f84ca8fe825f72
DiegoGp227 <diego.gp227@gmail.com>
2025-11-12 00:49:58 -0500

    [ build ] build aplicated

--- front/src/shared/constants/urls.ts
+++ front/src/shared/constants/urls.ts
@@ -1,4 +1,4 @@
 export const BaseURL =
-  process.env.NEXT_PUBLIC_API_URL || "";
+  process.env.NEXT_PUBLIC_API_URL || "https://api-coinmaster.devdiego.work";
 
 export const LoginURL = new URL("/api/login", BaseURL);
