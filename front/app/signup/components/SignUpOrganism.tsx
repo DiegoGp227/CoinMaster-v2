18f17fb12605422f530f421bd0b2f650be85f17c
DiegoGp227 <diego.gp227@gmail.com>
2025-11-11 20:24:57 -0500

    [ style ] minior changes

--- front/app/signup/components/SignUpOrganism.tsx
+++ front/app/signup/components/SignUpOrganism.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { currencies } from "@/src/data/data";
+import Link from "next/link";
 import { useState } from "react";
 import { LuSend } from "react-icons/lu";
 
@@ -11,7 +12,7 @@ export default function SignUpOrganism() {
     <div className="flex justify-center items-center min-h-screen w-screen gap-5">
       <form
         action=""
-        className="group border-2 border-soft-gray flex flex-col w-96 bg-hard-gray justify-center items-center p-10 gap-5 transition-colors duration-500
+        className="group border-2 border-soft-gray flex flex-col w-96 bg-hard-gray justify-center items-center p-5 gap-5 transition-colors duration-500
                            hover:border-white focus-within:border-white"
       >
         <h1 className="text-4xl font-bold text-soft-gray transition-colors duration-500 group-hover:text-white group-focus-within:text-white">
@@ -19,7 +20,7 @@ export default function SignUpOrganism() {
         </h1>
 
         {/* Username */}
-        <div className="group/field flex flex-col items-center">
+        <div className="group/field w-full flex flex-col items-center">
           <label
             htmlFor="username"
             className="text-soft-gray mb-1 transition-colors duration-500 group-focus-within/field:text-white group-hover/field:text-white"
@@ -29,13 +30,13 @@ export default function SignUpOrganism() {
           <input
             id="username"
             type="text"
-            className="px-2 py-2 bg-black text-white w-[350px] h-11 rounded-[5px] border-2 border-soft-gray
+            className="px-2 py-2 bg-black text-white w-full h-11 rounded-[5px] border-2 border-soft-gray
                                hover:border-white focus:border-white focus:outline-none transition-colors duration-500"
           />
         </div>
 
         {/* Email */}
-        <div className="group/field flex flex-col items-center">
+        <div className="group/field w-full flex flex-col items-center">
           <label
             htmlFor="email"
             className="text-soft-gray mb-1 transition-colors duration-500 group-focus-within/field:text-white group-hover/field:text-white"
@@ -45,13 +46,13 @@ export default function SignUpOrganism() {
           <input
             id="email"
             type="email"
-            className="px-2 py-2 bg-black text-white w-[350px] h-11 rounded-[5px] border-2 border-soft-gray
+            className="px-2 py-2 bg-black text-white w-full h-11 rounded-[5px] border-2 border-soft-gray
                                hover:border-white focus:border-white focus:outline-none transition-colors duration-500"
           />
         </div>
 
         {/* Password */}
-        <div className="group/field flex flex-col items-center">
+        <div className="group/field w-full flex flex-col items-center">
           <label
             htmlFor="password"
             className="text-soft-gray mb-1 transition-colors duration-500 group-focus-within/field:text-white group-hover/field:text-white"
@@ -61,13 +62,13 @@ export default function SignUpOrganism() {
           <input
             id="password"
             type="password"
-            className="px-2 py-2 bg-black text-white w-[350px] h-11 rounded-[5px] border-2 border-soft-gray
+            className="px-2 py-2 bg-black text-white w-full h-11 rounded-[5px] border-2 border-soft-gray
                                hover:border-white focus:border-white focus:outline-none transition-colors duration-500"
           />
         </div>
 
         {/* Currency */}
-        <div className="group/field flex flex-col items-center">
+        <div className="group/field w-full flex flex-col items-center">
           <label
             htmlFor="currency"
             className="text-soft-gray mb-1 transition-colors duration-500 group-focus-within/field:text-white group-hover/field:text-white"
@@ -78,7 +79,7 @@ export default function SignUpOrganism() {
             id="currency"
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
-            className="px-2 py-2 bg-black text-white w-[350px] h-11 rounded-[5px] border-2 border-soft-gray hover:border-white focus:border-white focus:outline-none transition-colors duration-500"
+            className="px-2 py-2 bg-black text-white w-full h-11 rounded-[5px] border-2 border-soft-gray hover:border-white focus:border-white focus:outline-none transition-colors duration-500"
           >
             {currencies.map(({ code, name, symbol }) => (
               <option key={code} value={code}>
@@ -92,11 +93,12 @@ export default function SignUpOrganism() {
         <button
           className="bg-black text-soft-gray w-[150px] h-11 border-2 border-soft-gray 
                              hover:border-white hover:text-white focus:border-white flex justify-center items-center gap-2 py-5 
-                             transition-colors duration-500"
+                             transition-colors duration-500 mt-5"
         >
           Send
           <LuSend />
         </button>
+        <Link href="/login" className="text-soft-gray font-bold hover:text-white transition-colors duration-500 group">Login</Link>
       </form>
     </div>
   );
