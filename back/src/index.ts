6dc1f59bb9e0f0fd235d871740e2ddefac1fc46e
DiegoGp227 <diego.gp227@gmail.com>
2025-11-29 01:56:21 -0500

    [ fix ] docker fix

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import { router } from "../routes/index.routes.js";
 
+(BigInt.prototype as any).toJSON = function() {
+  return this.toString();
+};
+
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4001;
 
 const app = express();
